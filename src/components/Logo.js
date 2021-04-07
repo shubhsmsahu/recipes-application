c6f479b649f14eee8c37b6b4c273600253a84843
Shubham Sahu <75929395+shubhsmsahu@users.noreply.github.com>
2021-04-07 16:21:15 +0530

    Urlchange/

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to="https://recipes-application.vercel.app/" className="logo">
+    <Link to="/" className="logo">
       Recipes<span>.</span>
     </Link>
   );
